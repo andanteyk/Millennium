3a0b5297f3b9f22bc4998fbc50fa7a2f05c8c075
Andante <andante.soft@gmail.com>
2021-05-01 07:52:04 +0900

    被弾実装

--- Assets/Millennium/Scripts/InGame/Entity/Player/Player.cs
+++ Assets/Millennium/Scripts/InGame/Entity/Player/Player.cs
@@ -1,5 +1,9 @@
 using Cysharp.Threading.Tasks;
+using Cysharp.Threading.Tasks.Linq;
+using Millennium.InGame.Effect;
 using Millennium.IO;
+using Millennium.Sound;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
@@ -10,81 +14,142 @@ namespace Millennium.InGame.Entity.Player
 {
     public class Player : EntityLiving
     {
+        [SerializeField]
+        private GameObject m_BulletPrefab;
+        [SerializeField]
+        private GameObject m_BombPrefab;
+
+        [SerializeField]
+        private float m_ShotInterval = 0.25f;
+
+        [SerializeField]
+        private float m_MoveSpeed = 64;
 
 
-        private bool IsInvincible => InvincibleTime > 0;
-        private float InvincibleTime { get; set; }
+        private bool IsInvincible => Time.fixedTime < m_InvincibleUntil;
+        private float m_InvincibleUntil = 0;
+
+        private int m_BombCount = 3;
+
+        private bool IsControllable => Health > 0;
 
 
 
         // Start is called before the first frame update
-        async void Start()
+        private void Start()
         {
+            HealthMax = 300;       // TODO
             Health = HealthMax;
 
-            async UniTask OnStart(CancellationToken token)
-            {
-                var input = new InputControls();
-                input.Enable();
+            var input = new InputControls();
+            input.Enable();
 
-                var bulletPrefab = await Addressables.LoadAssetAsync<GameObject>("Assets/Millennium/Assets/Prefabs/InGame/Bullet/PlayerBullet.prefab");
-                var bombPrefab = await Addressables.LoadAssetAsync<GameObject>("Assets/Millennium/Assets/Prefabs/InGame/Bullet/AliceBomb Variant.prefab");
+            //var bulletPrefab = await Addressables.LoadAssetAsync<GameObject>("Assets/Millennium/Assets/Prefabs/InGame/Bullet/PlayerBullet.prefab");
+            //var bombPrefab = await Addressables.LoadAssetAsync<GameObject>("Assets/Millennium/Assets/Prefabs/InGame/Bullet/AliceBomb Variant.prefab");
 
-                var rigidbody = GetComponent<Rigidbody2D>();
-
-                var coolTime = 0f;
-                var coolTimeMax = 0.25f;
+            var rigidbody = GetComponent<Rigidbody2D>();
+            var renderer = GetComponent<SpriteRenderer>();
 
 
-                input.Player.Bomb.started += context =>
+            var token = this.GetCancellationTokenOnDestroy();
+
+            // input loop - shot
+            UniTaskAsyncEnumerable.EveryUpdate(PlayerLoopTiming.FixedUpdate)
+                .ForEachAwaitAsync(async _ =>
                 {
-                    var bullet = Instantiate(bombPrefab);
+                    if (!IsControllable)
+                        return;
+                    if (!input.Player.Fire.IsPressed())
+                        return;
+
+                    var bullet = Instantiate(m_BulletPrefab);
                     bullet.transform.position = transform.position;
-                };
 
-                await UniTask.Yield(PlayerLoopTiming.FixedUpdate);
+                    await UniTask.Delay(TimeSpan.FromSeconds(m_ShotInterval));
+                }, token);
 
-                while (!token.IsCancellationRequested)
+            // input loop - bomb
+            UniTaskAsyncEnumerable.EveryUpdate()
+                .ForEachAwaitAsync(async _ =>
                 {
-                    // note: 物理挙動周りは FixedUpdate でやる
-                    // 入力が絡むもの (特に down/up イベントが必要なもの) は Update でやる
+                    if (!IsControllable)
+                        return;
+                    if (!input.Player.Bomb.WasPressedThisFrame())
+                        return;
 
-                    // move
-                    MoveByInput(rigidbody, input, 1);
-
-                    // shot
-                    if (coolTime <= 0)
+                    if (IsInvincible)
+                        return;
+                    if (m_BombCount <= 0)
                     {
-                        var bullet = Instantiate(bulletPrefab);
-                        bullet.transform.position = transform.position;
-
-                        coolTime += coolTimeMax;
+                        await SoundManager.I.PlaySe(SeType.Disabled);
+                        return;
                     }
-                    coolTime -= Time.deltaTime;
 
-                    await UniTask.Yield(PlayerLoopTiming.FixedUpdate);
-                }
-            }
+                    m_BombCount--;
 
-            await OnStart(this.GetCancellationTokenOnDestroy());
+                    SetInvincible(5);
+
+                    var bullet = Instantiate(m_BombPrefab);
+                    bullet.transform.position = transform.position;
+                }, token);
+
+            // input loop - move
+            UniTaskAsyncEnumerable.EveryUpdate(PlayerLoopTiming.FixedUpdate)
+                .ForEachAwaitAsync(async _ =>
+                {
+                    if (!IsControllable)
+                        return;
+
+                    var movedPosition = transform.position + (Vector3)input.Player.Direction.ReadValue<Vector2>() * m_MoveSpeed * Time.deltaTime;
+                    movedPosition = new Vector3(
+                        Mathf.Clamp(movedPosition.x, InGameConstants.PlayerFieldArea.xMin, InGameConstants.PlayerFieldArea.xMax),
+                        Mathf.Clamp(movedPosition.y, InGameConstants.PlayerFieldArea.yMin, InGameConstants.PlayerFieldArea.yMax));
+                    rigidbody.MovePosition(movedPosition);
+                }, token);
+
+            // display update
+            UniTaskAsyncEnumerable.EveryUpdate()
+                .ForEachAwaitAsync(async _ =>
+                {
+                    if (IsInvincible)
+                    {
+                        renderer.enabled = Time.time % (1 / 8f) < (1 / 8f / 2);
+                    }
+                    else
+                    {
+                        renderer.enabled = true;
+                    }
+                }, token);
         }
 
-
-        protected void MoveByInput(Rigidbody2D rigidbody, InputControls input, float scale)
-        {
-            var movedPosition = transform.position + (Vector3)input.Player.Direction.ReadValue<Vector2>() * 64 * Time.deltaTime;
-            movedPosition = new Vector3(
-                Mathf.Clamp(movedPosition.x, InGameConstants.PlayerFieldArea.xMin, InGameConstants.PlayerFieldArea.xMax),
-                Mathf.Clamp(movedPosition.y, InGameConstants.PlayerFieldArea.yMin, InGameConstants.PlayerFieldArea.yMax));
-            rigidbody.MovePosition(movedPosition);
-        }
 
         public override void DealDamage(DamageSource damage)
         {
+            if (IsInvincible)
+            {
+                return;
+            }
+
+
             Health -= damage.Damage;
 
-            // hit register(i.e. invincibl-ize)
-            // TODO: death
+            if (Health > 0)
+            {
+                // damaged - invinsiblize, bomb?
+                SetInvincible(5);
+
+                SoundManager.I.PlaySe(SeType.PlayerDamaged).Forget();
+            }
+            else
+            {
+                // TODO: game over
+                EffectManager.I.Play(EffectType.Explosion, transform.position);
+            }
+        }
+
+        private void SetInvincible(float seconds)
+        {
+            m_InvincibleUntil = Time.fixedTime + seconds;
         }
     }
 }
